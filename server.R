8d1e59a24952efffaeb320587cd29fbd8e8a47fe
Harlan Harris <harlan@harris.name>
2014-05-14 22:12:13 -0400

    switch to navbar; add by-ward plot

--- server.R
+++ server.R
@@ -21,23 +21,39 @@ dat <- subset(dat, !is.na(setasides_value))
 
 datc <- subset(dat, select=c(boundaries_value, feederpatterns_value, eschoicesets_value,
                              mschoicesets_value, msproximity_value, hslottery_value,
-                             setasides_value))
+                             setasides_value, ward))
 datc <- datc[complete.cases(datc), ]
+colnames(datc) <- c("Boundary Change",
+                        "Feeder Pattern Change",
+                        "Elementary Choice Set",
+                        "Middle Choice Set",
+                        "Two Closest Middle",
+                        "Citywide High School Lottery",
+                        "Set-Asides for Low-Perf Areas", "Ward")
+
+datc_long <- melt(cbind(datc, resp=rownames(datc)), variable.name='Question', value.name='Response')
+datc_long <- subset(datc_long, Response>0)
+datc_long <- mutate(datc_long,
+                    Ward=as.numeric(str_extract(Ward, "\\d")))
+
 datc_pam <- pam(datc, k=3)
 
 patterns <- datc_pam$medoids
-colnames(patterns) <- c("Boundary Change",
-  "Feeder Pattern Change",
-  "Elementary Choice Set",
-  "Middle Choice Set",
-  "Two Closest Middle",
-  "Citywide High School Lottery",
-  "Set-Asides for Low-Perf Areas")
-patterns <- as.data.frame(patterns)
-patterns$id <- 1:nrow(patterns)
 
-patterns <- melt(as.data.frame(patterns), id.vars='id', variable.name='Question', value.name='Response')
-patterns$id <- factor(patterns$id)
+patterns <- as.data.frame(patterns)
+patterns$id <- rownames(patterns)
+
+patterns <- melt(patterns, id.vars='id', variable.name='Question', value.name='Response')
+
+# ggplot(datc_long, aes(Question, Response, group=resp, color=Ward)) + 
+#     geom_line(alpha=.2, position='jitter') +
+#     coord_flip() + 
+#     theme_bw() + theme(legend.position='none')
+
+Mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+}
 
 shinyServer(function(input, output) {
    
@@ -51,10 +67,13 @@ shinyServer(function(input, output) {
   })
   
   output$patternPlot <- renderPlot({
-      pl <- ggplot(patterns, aes(Question, Response, group=id, color=id)) +
-          geom_line() + 
-          geom_point() +
-          coord_flip()
+      dat_to_plot <- if (input$ward == 'All') datc_long else subset(datc_long, Ward==input$ward)
+      pl <- ggplot(dat_to_plot, aes(Question, Response, group=resp)) + 
+          geom_line(alpha=.2, position='jitter') +
+          stat_summary(aes(group=NULL), fun.y=Mode, geom='point', size=3, color='red') + 
+          coord_flip() + 
+          theme_bw() + theme(legend.position='none')
+      print(pl)
       
   })
 })
--- ui.R
+++ ui.R
@@ -7,25 +7,35 @@
 
 library(shiny)
 
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel("Our DC Schools"),
-  
-  # Sidebar with a radio button for question
-  sidebarPanel(
-    radioButtons("question", "Survey Question:",
-                 c("Boundary Change"="boundaries_value",
-                   "Feeder Pattern Change"="feederpatterns_value",
-                   "Elementary Choice Set"="eschoicesets_value",
-                   "Middle Choice Set"="mschoicesets_value",
-                   "Two Closest Middle"="msproximity_value",
-                   "Citywide High School Lottery"="hslottery_value",
-                   "Set-Asides for Low-Perf Areas"="setasides_value"))
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("wardHistPlot", height="600px")
-  )
+shinyUI(navbarPage("Our DC Schools",
+    tabPanel("Questions",
+             sidebarLayout(
+                 # Sidebar with a radio button for question
+                 sidebarPanel(
+                     radioButtons("question", "Survey Question:",
+                                  c("Boundary Change"="boundaries_value",
+                                    "Feeder Pattern Change"="feederpatterns_value",
+                                    "Elementary Choice Set"="eschoicesets_value",
+                                    "Middle Choice Set"="mschoicesets_value",
+                                    "Two Closest Middle"="msproximity_value",
+                                    "Citywide High School Lottery"="hslottery_value",
+                                    "Set-Asides for Low-Perf Areas"="setasides_value"))
+                 ),
+                 
+                 # Show a plot of the generated distribution
+                 mainPanel(
+                     plotOutput("wardHistPlot", height="600px")
+                 )
+            )),
+    tabPanel("Wards",
+             sidebarLayout(
+                 sidebarPanel(
+                     radioButtons("ward", "Ward:",
+                                  c("All", "1", "2", "3", "4", "5", "6", "7", "8"))
+                ),
+                mainPanel(
+                    plotOutput("patternPlot", height="600px")
+                )
+            ))
+    
 ))
