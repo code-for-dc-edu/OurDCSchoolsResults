70c14e0fa8acbb9fe347b545d07974d70b3849a6
Harlan Harris <harlan@harris.name>
2014-05-17 11:27:24 -0400

    graph improvements

--- server.R
+++ server.R
@@ -62,16 +62,20 @@ shinyServer(function(input, output) {
     # generate and plot an rnorm distribution with the requested
     # number of observations
     pl <- ggplot(dat, aes_string(x=input$question)) +
-        geom_histogram(binwidth=1) + facet_wrap(~ward, scales='free') + xlim(1,6)
+        geom_histogram(binwidth=1) + facet_wrap(~ward, scales='free') + 
+        scale_x_continuous("", breaks=(1:5)+.5, limits=c(1,6), labels=c('Frown', '', 'Neutral', '', 'Smile'))+ 
+        ylab("# of Responses") + theme_bw()
     print(pl)
   })
   
   output$patternPlot <- renderPlot({
       dat_to_plot <- if (input$ward == 'All') datc_long else subset(datc_long, Ward==input$ward)
       pl <- ggplot(dat_to_plot, aes(Question, Response, group=resp)) + 
-          geom_line(alpha=.2, position='jitter') +
-          stat_summary(aes(group=NULL), fun.y=Mode, geom='point', size=3, color='red') + 
-          coord_flip() + 
+          geom_line(alpha=.2, position=position_jitter(w=.2, h=.2)) +
+          stat_summary(aes(group=NULL), fun.y=Mode, geom='point', size=5, color='red') + 
+          scale_y_continuous("",  breaks=(1:5), labels=c('Frown', '', 'Neutral', '', 'Smile'))+ 
+          scale_x_discrete("") + 
+          coord_flip(ylim=c(.5,5.5)) + 
           theme_bw() + theme(legend.position='none')
       print(pl)
       
